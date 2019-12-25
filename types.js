7b70faf8a23f43116c792b74d9fb1f2ae4608607
Marigi <marigindegwa@gmail.com>
2019-12-25 08:20:57 +0300

    1. learning Variable types in JavaScript  a. String  b. Numbers  c. Boolean  d. Object  e. Null  f. Undefined  g. Symbol

---
+++ types.js
@@ -0,0 +1,20 @@
+const name = 'benji';
+const middle = 'marigi';
+const last = 'ndegwa';
+
+const sentence = 'she\'s my "kind" of girl';
+
+const hello = `Hello My name is ${name} and I'm ${38} years old`;
+
+const smarties = 20;
+const kids = 3;
+
+const eachKidsGets = Math.floor(smarties / kids);
+
+const person = {
+  name: 'benjamin',
+  middle: 'Marigi',
+  last: 'Ndegwa',
+};
+
+console.log(`Each kid will get ${person.last}`);
